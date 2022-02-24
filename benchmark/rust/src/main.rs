6e5e8f5f07ca50f49e4a0d14a2f2661b150a473e
Tarık Yılmaz <tarikyilmaz@yandex.com.tr>
2022-02-24 15:51:26 +0300

    Update rust benchmark

--- benchmark/rust/src/main.rs
+++ benchmark/rust/src/main.rs
@@ -8,9 +8,10 @@ use rocksdb::DB;
 fn main() {
     let size = 10_000;
     let p = function::PREFIX;
+    let path = "/tmp/test";
 
     // Get RocksDB connection
-    let db = get_db();
+    let db = get_db(path);
 
     let get_key_f = |db: &DB, size: i32| get(db, format!("{}_{}", p, size).as_str());
     let put_key_f = |db: &DB, size: i32| put(db, format!("{}_{}", p, size).as_str(), "value");
@@ -18,6 +19,8 @@ fn main() {
     let get_key_diff = timeit(get_key_f, size, &db);
     let put_key_diff = timeit(put_key_f, size, &db);
 
-    println!("Put key took {}μs", put_key_diff);
-    println!("Get key took {}μs", get_key_diff);
+    println!("Put key = {}μs", put_key_diff);
+    println!("Get key = {}μs", get_key_diff);
+
+    destroy(db, path);
 }
