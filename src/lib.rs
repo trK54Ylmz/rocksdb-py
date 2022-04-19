4e6e30892030fa2b08098ef15c972f340bb7610b
Tarık Yılmaz <tarikyilmaz@yandex.com.tr>
2022-04-19 21:46:18 +0300

    Add open as secondary and read only

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,8 @@ fn rocksdbpy(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(open, m)?).unwrap();
     m.add_function(wrap_pyfunction!(open_default, m)?).unwrap();
     m.add_function(wrap_pyfunction!(open_with_ttl, m)?).unwrap();
+    m.add_function(wrap_pyfunction!(open_for_readonly, m)?).unwrap();
+    m.add_function(wrap_pyfunction!(open_as_secondary, m)?).unwrap();
     m.add_function(wrap_pyfunction!(destroy, m)?).unwrap();
 
     m.add("RocksDBException", py.get_type::<RocksDBPyException>())?;
