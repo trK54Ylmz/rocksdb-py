c58790f2c7388decbef1df48a453073ec2fe2ce4
dieperdev <140034404+dieperdev@users.noreply.github.com>
2025-11-19 20:39:04 -0500

    add optimize_level_style_compaction option

--- src/option.rs
+++ src/option.rs
@@ -290,4 +290,27 @@ impl OptionPy {
     pub fn increase_parallelism(&mut self, parallelism: i32) {
         self.inner.increase_parallelism(parallelism)
     }
+
+    /// Optimize level style compaction.
+    /// 
+    /// Default values for some parameters in `Options`` are not optimized for heavy workloads and
+    /// big datasets, which means you might observe write stalls under some conditions.
+    /// 
+    /// This can be used as one of the starting points for tuning RocksDB options in such cases.
+    /// 
+    /// Internally, it sets `write_buffer_size`, `min_write_buffer_number_to_merge`,
+    /// `max_write_buffer_number`, `level0_file_num_compaction_trigger`, `target_file_size_base`,
+    /// `max_bytes_for_level_base`, so it can override if those parameters were set before.
+    ///
+    /// It sets buffer sizes so that memory consumption would be constrained by `memtable_memory_budget`.
+    ///
+    /// Default: `0x4000000`
+    ///
+    /// Examples
+    /// ```
+    /// opts.optimize_level_style_compaction(64 * 1024 * 1024)
+    /// ```
+    pub fn optimize_level_style_compaction(&mut self, memtable_memory_budget: usize) {
+        self.inner.optimize_level_style_compaction(memtable_memory_budget)
+    }
 }
